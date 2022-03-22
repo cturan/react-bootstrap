f0cd197047ac19468c338161ded1dd0d11c8cad9
Çetin <cturans@gmail.com>
2022-03-23 01:12:04 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./styles.css";
+
+import "bootstrap/dist/css/bootstrap.css";
+
+import Button from "react-bootstrap/Button";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Tooltip from "react-bootstrap/Tooltip";
+
+export default function App() {
+  const renderTooltip = props => (
+    <Tooltip {...props}>Tooltip for the register button</Tooltip>
+  );
+
+  return (
+    <div className="App">
+      <OverlayTrigger placement="top" overlay={renderTooltip}>
+        <Button>Register</Button>
+      </OverlayTrigger>
+    </div>
+  );
+}
